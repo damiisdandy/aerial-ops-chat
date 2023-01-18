781ec5cd2abf1c58dfb5ed959e2faf6bc10ede5c
Damilola Jerugba <damiisdandy@icloud.com>
2023-01-18 23:18:11 +0100

    adjusted env variables and build script

--- src/utils/aws.ts
+++ src/utils/aws.ts
@@ -1,9 +1,8 @@
-import { Image } from '@prisma/client';
 import S3 from 'aws-sdk/clients/s3';
 
 const s3 = new S3({
   apiVersion: "2006-03-01",
-  accessKeyId: process.env.AWS_ACCESS_KEY,
+  accessKeyId: process.env.AWS_ACCESS,
   secretAccessKey: process.env.AWS_SECRET,
   region: process.env.AWS_BUCKET_REGION,
   signatureVersion: "v4",

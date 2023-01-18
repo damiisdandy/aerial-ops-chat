66570ea38a02cd086953eb6e6ef1e51a672968ba
Damilola Jerugba <damiisdandy@icloud.com>
2023-01-18 23:11:58 +0100

    updated env variables

--- src/utils/aws.ts
+++ src/utils/aws.ts
@@ -4,8 +4,8 @@ import S3 from 'aws-sdk/clients/s3';
 const s3 = new S3({
   apiVersion: "2006-03-01",
   accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_KEY,
-  region: process.env.AWS_REGION,
+  secretAccessKey: process.env.AWS_SECRET,
+  region: process.env.AWS_BUCKET_REGION,
   signatureVersion: "v4",
 });
 
